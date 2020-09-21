73061969a88785dcb394e3743a1854013584ac4c
David Andres Sanchez Umbarila <d.sanchezu@uniandes.edu.co>
2020-09-21 12:04:58 -0500

    .

--- App/controller.py
+++ App/controller.py
@@ -1,3 +1,29 @@
+"""
+ * Copyright 2020, Departamento de sistemas y Computación
+ * Universidad de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ """
+
+import config as cf
+from App import model
+import csv
+
 
 """
 El controlador se encarga de mediar entre la vista y el modelo.
@@ -6,9 +32,6 @@ el modelo varias veces o integrar varias de las respuestas
 del modelo en una sola respuesta. Esta responsabilidad
 recae sobre el controlador.
 """
-import config as cf 
-from App import model
-import csv
 
 # ___________________________________________________
 #  Inicializacion del catalogo
@@ -38,27 +61,19 @@ def loadMovies(catalog, booksfile):
     input_file = csv.DictReader(open(booksfile1,encoding='utf-8-sig'))
     for movie in input_file:
         model.addMovie(catalog, movie)
-<<<<<<< HEAD
         genres=movie['genres'].split('|')
         for genre in genres:
             model.addMovieGenre(catalog, genre, movie)
 
  
-=======
-    return catalog
->>>>>>> 94d4c5c12ec1dc2ae8adb29736b192f650492e80
 
 def loadData(catalog, detailsfile, castingfile):
     """
     Carga los datos de los archivos en el modelo
     """
-<<<<<<< HEAD
-    loadMovies(catalog, detailsfile)
-=======
     
     catalog=loadMovies(catalog, detailsfile)
     return catalog
->>>>>>> 94d4c5c12ec1dc2ae8adb29736b192f650492e80
 
 def getMoviesByCompany(catalog,company_name):
     return model.getMoviesByCompany(catalog,company_name)
@@ -75,7 +90,4 @@ def size(lst):
 # ___________________________________________________
 #  Funciones de Estetica
 # ___________________________________________________
-<<<<<<< HEAD
 
-=======
->>>>>>> 94d4c5c12ec1dc2ae8adb29736b192f650492e80
